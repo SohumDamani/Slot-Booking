4a975662fca5d1256aef2e4cd0e665110246e92c
SohumDamani <damanisohum@gmail.com>
2022-04-15 12:18:05 +0530

    Manager can create new room and move to adding timeslot

--- main/manager/urls.py
+++ main/manager/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.manager,name='manager')
+    path('',views.manager,name='manager'),
+    path('create-room/',views.createRoom,name='create_room'),
+    path('time-slot/<str:pk>/',views.addTimeSlot,name='time_slot'),
 ]
--- main/manager/views.py
+++ main/manager/views.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render
-from .models import Rooms
+from django.shortcuts import render,redirect
+from .models import Rooms,TimeSlot,AdvanceBooking
+from .forms import RoomForm,TimeSlotForm
+from django.contrib import messages
 
 
 def manager(request):
     rooms = Rooms.objects.all()
-    context={'rooms':rooms}
+    timeslot = TimeSlot.objects.all()
+    advancebooking = AdvanceBooking.objects.all()
+    context={'rooms':rooms,'timeslot':timeslot,'advancebooking':advancebooking}
     return render(request,'manager/manager.html',context)
+
+def createRoom(request):
+    user = request.user
+    print("Username : ",user)
+    form = RoomForm()
+    if request.method=="POST":
+        form = RoomForm(request.POST)
+        form.instance.room_owner = user
+        try:
+            form.is_valid()
+            form.save()
+            return redirect('manager')
+        except:
+            messages.error(request,"Room Already Exist!")
+            return redirect('create_room')
+    context={'form':form,'user':user}
+    return render(request,'manager/create_room.html',context)
+
+# def updateRoom(request,pk):
+#     room = Rooms.objects.get(id=pk)
+#     context={}
+#     return render(request,'manager/edit-room.html',context)
+
+def addTimeSlot(request,pk):
+    user = request.user
+    form = TimeSlotForm()
+    context={'form':form}
+    return render(request,'manager/add_time_slot.html',context)
