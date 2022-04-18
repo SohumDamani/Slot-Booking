4e207dc45512a035793184807d2ef17703de0697
SohumDamani <damanisohum@gmail.com>
2022-04-18 15:12:20 +0530

    Removed Debug mode

---
+++ main/account/decorators.py
@@ -0,0 +1,13 @@
+
+from django.core.exceptions import PermissionDenied
+
+def role_required(allowed=[]):
+    def decorator(view_func):
+        def wrap(request,*args,**kwargs):
+            if request.user.is_anonymous:
+                return view_func(request,*args,**kwargs)
+            else:
+                raise PermissionDenied
+
+        return wrap
+    return decorator
